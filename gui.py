e79ca0efa74b5dbcef96259a4d42e503c7809242
m.ue.d <81645900+m-ue-d@users.noreply.github.com>
2023-08-19 20:18:51 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -6,12 +6,14 @@ import numpy
 # black: #000000
 # white: #FFFFFF
 
-X = 1200
-Y = 800
+X = 800
+Y = 480
 CENTER = (X // 2, Y // 2)
 SCREEN = None
 FONT = None
+FONT_SIZE = 80
 FONT_SMALL = None
+FONT_SIZE_SMALL = 25
 
 
 class Colors:
@@ -26,14 +28,12 @@ def text_with_blooming(c: str, rotation, radius, color, font):
     return text
 
 
-def initGui(x, y):
+def initGui():
     global SCREEN, FONT_SMALL, FONT, X, Y
-    X = x
-    Y = y
     pygame.init()
     SCREEN = pygame.display.set_mode((X, Y))
-    FONT = pygame.font.Font("OxygenMono-Regular.ttf", 200)
-    FONT_SMALL = pygame.font.Font("OxygenMono-Regular.ttf", 30)
+    FONT = pygame.font.Font("OxygenMono-Regular.ttf", FONT_SIZE)
+    FONT_SMALL = pygame.font.Font("OxygenMono-Regular.ttf", FONT_SIZE_SMALL)
     pygame.display.set_caption("Portal Radio")
 
 
@@ -46,37 +46,50 @@ def handleGui(potty, mode, special):
     SCREEN.fill((0, 0, 0))
 
     # potty code
+    final_text = "{:04.1f} FM".format(potty)
+    if mode == "Alarm":
+        final_text = special
 
-    final_text = "{:.1f} FM".format(potty)
+    offset_multiplier_x = 50
+    offset_multiplier_y = 4
+    rotation_multiplier = 4
 
     for i, c in enumerate(final_text):
-        text_back = text_with_blooming(c, -(i * 10 - len(final_text) * 10 / 2), 12, Colors.DARK, FONT)
-        text_fore = text_with_blooming(c, -(i * 10 - len(final_text) * 10 / 2), 4, Colors.BRIGHT, FONT)
+        rotation = -2 * (i - (len(final_text)-1)/2) * rotation_multiplier
+        text_fore = text_with_blooming(c, rotation, 2, Colors.BRIGHT, FONT)
+        text_back = text_with_blooming(c, rotation, 12, Colors.DARK, FONT)
+        x = CENTER[0] + i * offset_multiplier_x - (len(final_text) * offset_multiplier_x - FONT_SIZE/2) / 2
+        y = CENTER[1] + numpy.power(i - (len(final_text)-1)/2, 2) * offset_multiplier_y
         text_rect = text_fore.get_rect()
-
-        x = CENTER[0] + i * 120 - (len(final_text) * 120 - 60) / 2
-        y = CENTER[1] + numpy.power(x - X / 2 + 30, 2) / X
         text_rect.center = (x, y)
         SCREEN.blit(text_back, text_rect)
         SCREEN.blit(text_fore, text_rect)
 
+
     # mode code
-    off_x = 140
-    off_y = 25
-    text_mode_fore = text_with_blooming(mode, 75, 2, Colors.BRIGHT, FONT_SMALL)
-    text_mode_back = text_with_blooming(mode, 75, 8, Colors.DARK, FONT_SMALL)
+    x = CENTER[0] + offset_multiplier_x - (len(final_text) * offset_multiplier_x - FONT_SIZE/2) / 2
+    y = CENTER[1] + numpy.power(1 - (len(final_text)-1)/2, 2) * offset_multiplier_y + 60
+    rotation = -2 * (1 - (len(final_text)-1)/2) * rotation_multiplier
+    text_mode_fore = text_with_blooming(mode, rotation, 2, Colors.BRIGHT, FONT_SMALL)
+    text_mode_back = text_with_blooming(mode, rotation, 8, Colors.DARK, FONT_SMALL)
     text_mode_rect = text_mode_fore.get_rect()
-    text_mode_rect.center = (CENTER[0] + off_x, CENTER[1] + off_y)
+    text_mode_rect.center = (x, y)
     SCREEN.blit(text_mode_fore, text_mode_rect)
     SCREEN.blit(text_mode_back, text_mode_rect)
 
     # special
-    off_x = 0
-    off_y = 135
-    text_mode_fore = text_with_blooming(special, 0, 2, Colors.BRIGHT, FONT_SMALL)
-    text_mode_back = text_with_blooming(special, 0, 8, Colors.DARK, FONT_SMALL)
+    text = special
+    if mode == "Alarm":
+        text = "{:04.1f} FM".format(potty)
+
+    i = len(final_text)-2
+    x = CENTER[0] + i * offset_multiplier_x - (len(final_text) * offset_multiplier_x - FONT_SIZE / 2) / 2
+    y = CENTER[1] + numpy.power(i - (len(final_text) - 1) / 2, 2) * offset_multiplier_y + 60
+    rotation = -2 * (i - (len(final_text) - 1) / 2) * rotation_multiplier
+    text_mode_fore = text_with_blooming(text, rotation, 2, Colors.BRIGHT, FONT_SMALL)
+    text_mode_back = text_with_blooming(text, rotation, 8, Colors.DARK, FONT_SMALL)
     text_mode_rect = text_mode_fore.get_rect()
-    text_mode_rect.center = (CENTER[0] + off_x, CENTER[1] + off_y)
+    text_mode_rect.center = (x, y)
     SCREEN.blit(text_mode_fore, text_mode_rect)
     SCREEN.blit(text_mode_back, text_mode_rect)
 
